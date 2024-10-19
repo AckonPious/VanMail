8cd01510f494ba6cecf9135f26557802e3bc002b
AckonPious <ackonpious469@gmail.com>
2024-10-19 12:00:37 +0000

    feat(mail): update models and factory with priority level changes
    Modified mail/models.py and factory.py for priority level updates.
    Updated initial migration file (0001_initial.py).
    Added new migration cache for priority level changes (0002_alter_mail_priority_level).

--- mail/factory.py
+++ mail/factory.py
@@ -1,25 +1,20 @@
 from datetime import datetime
-from .models import MailBox
+from .models import MailBox 
 
 class MailIDFactory:
     @staticmethod
     def generate_mail_id(location_name):
-        current_year = datetime.now().year
-        location_name = location_name.lower()  # Normalize location name to lowercase
-        
-        # New prefix format: location_year_
-        prefix = f"{location_name}_{current_year}_"
-
-        # Query the latest mail_id that starts with the location and year
+        now = datetime.now()
+        current_year = now.year
+        day_of_year = now.timetuple().tm_yday
+        location_name = location_name.lower()
+        prefix = f"{location_name}_{current_year}_{day_of_year}_"
         latest_mail = MailBox.objects.filter(mail_id__startswith=prefix).order_by('-mail_id').first()
-
+        
         if latest_mail:
-            # Get the last number of the mail_id and increment it
             last_id_number = int(latest_mail.mail_id.split('_')[-1])
             new_id_number = last_id_number + 1
         else:
-            # If no mail found, start with 1
             new_id_number = 1
-
-        # Return the new mail_id formatted as location_year_1, 2, 3, etc.
         return f"{prefix}{new_id_number}"
+
--- mail/models.py
+++ mail/models.py
@@ -2,26 +2,21 @@ from django.db import models
 from authentication.models import User
 from authentication.models import Location 
 
-# Create your models here.
 class ModelInherit(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-
 class AssignedTo(ModelInherit):
     name = models.CharField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=15, null=True, blank=False)
     def __str__(self):
-        return self.name
-
+        return f"{self.name}" 
 
 MAIL_STATUS = [
         ('Drafted', 'Drafted'),
         ('In Transit', 'In Transit'),
         ('Received', 'Received'),
     ]  
-
-
 class MailBox(ModelInherit):
     mail_id = models.CharField(max_length=200, unique=True)
     handling_officer = models.CharField(max_length=100)
@@ -34,7 +29,7 @@ class MailBox(ModelInherit):
     created_by = models.ForeignKey(User, on_delete=models.PROTECT)  
 
     def __str__(self):
-        return self.mail_id
+        return f"{self.mail_id}" 
     
     def number_of_mails(self):
         return self.individual_mails.count()
@@ -44,10 +39,10 @@ class MailBox(ModelInherit):
 class Mail(ModelInherit):
     mail_box = models.ForeignKey(MailBox, on_delete=models.PROTECT, related_name='individual_mails')
     individual_mail_id = models.CharField(max_length=100, unique=True)  
-    mail_description = models.TextField()
-    priority_level = models.CharField(max_length=10, choices=[('Standard', 'Standard'), ('High', 'High')])
+    mail_description = models.TextField(blank=False, null=True)
+    priority_level = models.CharField(max_length=15, choices=[('Standard', 'Standard'), ('High Priority', 'High Priority')])
     created_by = models.ForeignKey(User, on_delete=models.PROTECT, related_name='created_by', blank=False) 
     is_received = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.individual_mail_id
+        return f"{self.individual_mail_id}" 
